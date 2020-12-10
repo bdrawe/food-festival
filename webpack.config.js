a3e1f7c29e7ae9e8993d9e9ef56c23e648c3484d
B. Drawe <brycerdrawe.b@gmail.com>
2020-12-10 00:29:28 -0700

    multiple entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
+const webpack = require("webpack")
 const path = require("path");
-const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: { path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'},
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-    ],
-    mode: 'development'
-}
+  entry:{
+      app:"./assets/js/script.js",
+      tickets:"./assets/js/tickets.js",
+      events:"./assets/js/events.js",
+      schedule: "./assets/js/schedule"
+  },
+  output: {
+      filename: "[name].bundle.js",
+      path: __dirname + "/dist",
+  },
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+  })
+  ],
+  mode: "development"
+};
